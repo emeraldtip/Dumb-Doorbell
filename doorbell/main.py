a95f7b5fd3641549a98dfc42ad475657f9759d23
unknown <emeraldtipyt@gmail.com>
2024-11-07 12:24:09 +0200

    Audio playing, but hella distorted

--- doorbell/main.py
+++ doorbell/main.py
@@ -1,7 +1,8 @@
-from machine import Pin, PWM, I2S
+import os
+import time
+from machine import Pin, I2S
 
-button_pin = Pin(23,Pin.IN,Pin.PULL_UP)
-buzzer_pin = PWM(Pin(22,Pin.OUT),1000)
+#button_pin = Pin(23,Pin.IN,Pin.PULL_UP)
 
 
 #Audio
@@ -10,18 +11,42 @@ ws_pin = Pin(13)    # Word clock output
 sd_pin = Pin(12)    # Serial data output
 
 
-audio_out = I2S(2,
-                sck=sck_pin, ws=ws_pin, sd=sd_pin,
-                mode=I2S.TX,
-                bits=16,
-                format=I2S.MONO,
-                rate=44100,
-                ibuf=20000)
+audio_out = I2S(0, sck=sck_pin, ws=ws_pin, sd=sd_pin,mode=I2S.TX, bits=16, format=I2S.MONO, rate=11025, ibuf=20000)
 
+# some code from from https://github.com/miketeachman/micropython-i2s-examples
+# The MIT License (MIT)
+# Copyright (c) 2022 Mike Teachman
+# https://opensource.org/licenses/MIT
+#
+# Purpose:  Play a WAV audio file out of a speaker or headphones
+#
+wav = open("yee.wav", "rb")
+pos = wav.seek(44)
 
+wav_samples = bytearray(1000)
+wav_samples_mv = memoryview(wav_samples)
 
 #while True:
 #	if button_pin.value() == 0:
 #		buzzer_pin.duty(200)
 #	else:
 #		buzzer_pin.duty(0)
+
+print("start playback")
+try:
+    while True:
+        num_read = wav.readinto(wav_samples_mv)
+        # end of WAV file?
+        if num_read == 0:
+            # end-of-file, advance to first byte of Data section
+            #_ = wav.seek(44)
+        else:
+            _ = audio_out.write(wav_samples_mv[:num_read])
+
+except (KeyboardInterrupt, Exception) as e:
+    print("caught exception {} {}".format(type(e).__name__, e))
+
+# cleanup
+wav.close()
+audio_out.deinit()
+print("Done")
