9d837d7b6e1e0ba385cd95984f802ad05f97bfdf
unknown <emeraldtipyt@gmail.com>
2024-11-11 21:39:07 +0200

    Audio no longer obnoxious

--- doorbell/main.py
+++ doorbell/main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import struct
 from machine import Pin, I2S
 
 #button_pin = Pin(23,Pin.IN,Pin.PULL_UP)
@@ -13,40 +14,37 @@ sd_pin = Pin(12)    # Serial data output
 
 audio_out = I2S(0, sck=sck_pin, ws=ws_pin, sd=sd_pin,mode=I2S.TX, bits=16, format=I2S.MONO, rate=11025, ibuf=20000)
 
-# some code from from https://github.com/miketeachman/micropython-i2s-examples
-# The MIT License (MIT)
-# Copyright (c) 2022 Mike Teachman
-# https://opensource.org/licenses/MIT
-#
-# Purpose:  Play a WAV audio file out of a speaker or headphones
-#
+
 wav = open("yee.wav", "rb")
 pos = wav.seek(44)
 
-wav_samples = bytearray(1000)
-wav_samples_mv = memoryview(wav_samples)
+wav_samples_final = memoryview(bytearray(2048))
 
 #while True:
 #	if button_pin.value() == 0:
 #		buzzer_pin.duty(200)
 #	else:
 #		buzzer_pin.duty(0)
-
 print("start playback")
-try:
-    while True:
-        num_read = wav.readinto(wav_samples_mv)
-        # end of WAV file?
-        if num_read == 0:
-            # end-of-file, advance to first byte of Data section
-            #_ = wav.seek(44)
-        else:
-            _ = audio_out.write(wav_samples_mv[:num_read])
 
-except (KeyboardInterrupt, Exception) as e:
-    print("caught exception {} {}".format(type(e).__name__, e))
+while True:
+    #read in 2048 bytes from the wav file
+    read_bytes = wav.read(2048)
+    if len(read_bytes) != 0:
+        #convert bytes to integers, reduce amplitude (-64) and the put into bytearray
+        #wav_samples = memoryview(byt
+        e = 0
+        for i in struct.unpack("B"*len(read_bytes),read_bytes):
+            #print(i)
+            wav_samples_final[e] = int(i-64)
+            #print(wav_samples_final[e])
+            e+=1
 
-# cleanup
+        audio_out.write(wav_samples_final)
+    else:
+        break
+
+
 wav.close()
 audio_out.deinit()
 print("Done")
