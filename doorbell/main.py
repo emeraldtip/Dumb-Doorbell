872e63accb0b1e42798ea4facfe582ee85470303
unknown <emeraldtipyt@gmail.com>
2024-11-12 15:06:49 +0200

    Added more comments

--- doorbell/main.py
+++ doorbell/main.py
@@ -4,20 +4,23 @@ import struct
 import asyncio
 from machine import Pin, I2S
 
+#Button
 button_pin = Pin(23,Pin.IN,Pin.PULL_UP)
-
 
 #Audio
 sck_pin = Pin(14)   # Serial clock output
 ws_pin = Pin(13)    # Word clock output
 sd_pin = Pin(12)    # Serial data output
 
+#file opening logic
 wav = open("yee.wav", "rb")
 
-
+#audio file playing logic
 async def play_ringtone():
+    #Open I2S stream
     audio_out = I2S(0, sck=sck_pin, ws=ws_pin, sd=sd_pin,mode=I2S.TX, bits=16, format=I2S.MONO, rate=11025, ibuf=20000)
     
+    #seek past the header of the file to the audio data
     pos = wav.seek(44)
     
     wav_samples_final = memoryview(bytearray(2048))
@@ -26,10 +29,10 @@ async def play_ringtone():
         #read in 2048 bytes from the wav file
         read_bytes = wav.read(2048)
         if len(read_bytes) != 0:
-            #convert bytes to integers, reduce amplitude (-64) and the put into bytearray
+            #convert bytes to integers, reduce amplitude (-32) and the put into bytearray
             e = 0
             for i in struct.unpack("B"*len(read_bytes),read_bytes):
-                wav_samples_final[e] = int(i-120)
+                wav_samples_final[e] = int(i-32)
                 e+=1
             print("writing")
             audio_out.write(wav_samples_final)
@@ -43,13 +46,18 @@ async def play_ringtone():
 
 async def check_button():
     if button_pin.value() == 0:
+        # reopen file
         wav = open("yee.wav", "rb")
+        # seek to beginning of data
         wav.seek(44)
-        await asyncio.sleep(1)
+        
+        print("registered")
+        #wait before registering next button press
+        await asyncio.sleep_ms(300)
         asyncio.create_task(play_ringtone())
-        print("gaming")
+        
     else:
-        print("no")
+        pass
 
 while True:
     asyncio.run(check_button())
